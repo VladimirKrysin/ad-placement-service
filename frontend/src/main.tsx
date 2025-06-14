8e9a7678999cd336bb7b8a77ac7d50ede44c3da0
Vladimir <vovakrysinn@gmail.com>
2025-06-14 17:09:06 +0300

    добавлен mantine

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme, Button } from '@mantine/core';
+import App from './App.tsx';
+
+const theme = createTheme({
+  components: {
+    Button: Button.extend({
+      defaultProps: {
+        variant: 'filled',
+      },
+    }),
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
